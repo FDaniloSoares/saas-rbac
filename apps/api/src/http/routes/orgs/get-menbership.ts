9e36be029ea8f4917a2c962a55efc09687b2fc5b
FDaniloSoares <fdanilosoares@gmail.com>
2026-04-22 23:51:32 -0300

    feat: add get membership

---
+++ apps/api/src/http/routes/orgs/get-menbership.ts
@@ -0,0 +1,45 @@
+import { auth } from '@/http/middlewares/auth';
+import type { ZodTypeProvider } from 'fastify-type-provider-zod';
+import type { FastifyInstance } from 'fastify/types/instance';
+import { z } from 'zod';
+import { Role } from '../../../../prisma/generated/client';
+
+export async function getMembership(app: FastifyInstance) {
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      'organizations/:slug/membership',
+      {
+        schema: {
+          tags: ['organizations'],
+          summary: 'Get user membership organization',
+          security: [{ bearerAuth: [] }],
+          params: z.object({
+            slug: z.string(),
+          }),
+          response: {
+            200: z.object({
+              membership: z.object({
+                id: z.uuid(),
+                role: Role,
+                organizationId: z.uuid(),
+              }),
+            }),
+          },
+        },
+      },
+      async (request, _reply) => {
+        const { slug } = request.params;
+        const { membership } = await request.getUserMembership(slug);
+
+        return {
+          membership: {
+            id: membership.role,
+            role: membership.role,
+            organizationId: membership.organizationId,
+          },
+        };
+      }
+    );
+}
