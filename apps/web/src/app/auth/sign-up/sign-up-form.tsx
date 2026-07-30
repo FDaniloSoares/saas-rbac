1c985b636df6291d36d6e6505c0352230cc258c7
FDaniloSoares <fdanilosoares@gmail.com>
2026-07-29 23:01:24 -0300

    feat: add sign-up form

--- apps/web/src/app/auth/sign-up/sign-up-form.tsx
+++ apps/web/src/app/auth/sign-up/sign-up-form.tsx
@@ -12,30 +12,44 @@ import { Separator } from '@/components/ui/separator';
 import { useFormState } from '@/hooks/use-form-state';
 
 import { signInWithGithub } from '../actions';
-import { signInWithEmailAndPassword } from './actions';
+import { signUpAction } from './actions';
 
-export default function SignInForm() {
+export default function SignUpForm() {
   const router = useRouter();
   const [{ success, message, errors }, handleSignIn, isPending] = useFormState(
-    signInWithEmailAndPassword,
+    signUpAction,
     () => {
-      router.push('/');
+      router.push('/auth/sign-in');
     }
   );
 
   return (
     <div className="w-full max-w-88 space-y-4">
       <form onSubmit={handleSignIn} className="space-y-4">
+        {success === false && message && (
+          <Alert variant="destructive">
+            <AlertTriangle className="size-4" />
+            <AlertTitle>Sign in faild!</AlertTitle>
+            <AlertDescription>
+              <p>{message}</p>
+            </AlertDescription>
+          </Alert>
+        )}
         <div className="space-y-1">
-          {success === false && message && (
-            <Alert variant="destructive">
-              <AlertTriangle className="size-4" />
-              <AlertTitle>Sign in faild!</AlertTitle>
-              <AlertDescription>
-                <p>{message}</p>
-              </AlertDescription>
-            </Alert>
+          <Label htmlFor="name">Name</Label>
+          <Input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name"
+          ></Input>
+          {errors?.name && (
+            <p className="text-xs font-medium text-red-50 dark:text-red-400">
+              {errors.name[0]}
+            </p>
           )}
+        </div>
+        <div className="space-y-1">
           <Label htmlFor="email">E-mail</Label>
           <Input
             type="email"
@@ -63,17 +77,26 @@ export default function SignInForm() {
               {errors.password[0]}
             </p>
           )}
-          <Link
-            href="/auth/forgot-password"
-            className="text-foregraund text-xs font-medium hover:underline"
-          >
-            Forgot your password?
-          </Link>
+        </div>
+
+        <div className="space-y-1">
+          <Label htmlFor="password_confirmation">Confirm password</Label>
+          <Input
+            type="password"
+            id="password_confirmation"
+            name="password_confirmation"
+            placeholder="Confirm your password"
+          ></Input>
+          {errors?.password_confirmation && (
+            <p className="text-xs font-medium text-red-50 dark:text-red-400">
+              {errors.password_confirmation[0]}
+            </p>
+          )}
         </div>
 
         <Button type="submit" className="w-full" disabled={isPending}>
           {isPending && <Loader2 className="animate-spin" />}
-          Sign in with e-mail
+          Create account
         </Button>
 
         <Button
@@ -81,9 +104,9 @@ export default function SignInForm() {
           className="w-full"
           size="xs"
           nativeButton={false}
-          render={<Link href="/auth/sign-up" />}
+          render={<Link href="/auth/sign-in" />}
         >
-          Create new account
+          Already registered? Sign in
         </Button>
       </form>
 
@@ -101,7 +124,7 @@ export default function SignInForm() {
           >
             <path d="M10.226 17.284c-2.965-.36-5.054-2.493-5.054-5.256 0-1.123.404-2.336 1.078-3.144-.292-.741-.247-2.314.09-2.965.898-.112 2.111.36 2.83 1.01.853-.269 1.752-.404 2.853-.404 1.1 0 1.999.135 2.807.382.696-.629 1.932-1.1 2.83-.988.315.606.36 2.179.067 2.942.72.854 1.101 2 1.101 3.167 0 2.763-2.089 4.852-5.098 5.234.763.494 1.28 1.572 1.28 2.807v2.336c0 .674.561 1.056 1.235.786 4.066-1.55 7.255-5.615 7.255-10.646C23.5 6.188 18.334 1 11.978 1 5.62 1 .5 6.188.5 12.545c0 4.986 3.167 9.12 7.435 10.669.606.225 1.19-.18 1.19-.786V20.63a2.9 2.9 0 0 1-1.078.224c-1.483 0-2.359-.808-2.987-2.313-.247-.607-.517-.966-1.034-1.033-.27-.023-.359-.135-.359-.27 0-.27.45-.471.898-.471.652 0 1.213.404 1.797 1.235.45.651.921.943 1.483.943.561 0 .92-.202 1.437-.719.382-.381.674-.718.944-.943" />
           </svg>
-          Sign in with Github
+          Sign up with Github
         </Button>
       </form>
     </div>
