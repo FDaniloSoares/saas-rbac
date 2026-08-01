e8d45287237033775fab86a9801046bbe4b20ed8
FDaniloSoares <fdanilosoares@gmail.com>
2026-07-31 22:06:57 -0300

    feat: next commit

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,7 +1,9 @@
+import { Slash } from 'lucide-react';
 import Image from 'next/image';
 
 import dragonLogo from '@/assets/dragon-logo.svg';
 
+import { OrganizationSwitcher } from './organization-switcher';
 import { ProfileButton } from './profile-button';
 
 export function Header() {
@@ -9,6 +11,9 @@ export function Header() {
     <div className="mx-auto flex max-w-300 items-center justify-between">
       <div className="flex items-center gap-3">
         <Image src={dragonLogo} className="size-6 dark:invert" alt="logo" />
+
+        <Slash className="text-border size-3 -rotate-24" />
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
---
+++ apps/web/src/components/organization-switcher.tsx
@@ -0,0 +1,41 @@
+import { ChevronsUpDown, PlusCircle } from 'lucide-react';
+import Link from 'next/link';
+
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+
+export async function OrganizationSwitcher() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="focus-visible:ring-primary flex w-42 items-center gap-2 rounded p-1 text-sm font-medium outline-none focus-visible:ring-2">
+        <span className="text-muted-foreground">Select organization</span>
+        <ChevronsUpDown className="ml-auto size-4" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-50" alignOffset={-16}>
+        <DropdownMenuGroup>
+          <DropdownMenuLabel>Organizations</DropdownMenuLabel>
+          <DropdownMenuItem>
+            <Avatar className="mr-2 size-5">
+              <AvatarImage src="https://github.com/fdanilosoares.png" />
+              <AvatarFallback />
+            </Avatar>
+            <span className="line-clamp-1">DRS Tech</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem render={<Link href="/create-organization" />}>
+          <PlusCircle className="mr-2 size-5" />
+          Create new
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
